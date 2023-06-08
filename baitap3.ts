9e7611aa14cdac4975a8c20e66a60d09e3615206
Vinh <phuocvinhpham96@gmail.com>
2023-06-08 10:39:22 +0700

    feature/baitap3

---
+++ baitap3.ts
@@ -0,0 +1,11 @@
+function isEven(n: number): boolean {
+    return n% 2 === 0 ;
+
+}
+const n=4;
+const isNEven =isEven(n);
+console.log(isNEven);  //true
+
+const m=7;
+const isMEven = isEven(m);
+console.log(isMEven); //false
